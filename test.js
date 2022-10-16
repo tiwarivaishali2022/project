b7e92599003fd588e7a80fc77a0f4f04216418d1
Vaishali Tiwari <tiwarivaishali2022@gmail.com>
2022-10-16 16:30:11 +0530

    commit first name

--- test.js
+++ test.js
@@ -1,1 +1,9 @@
-console.log("Hello")
+var firstname = "Vaishali";
+var lastname = "Tiwari";
+var address = "Chanda";
+console.log("Hello",firstname,lastname,address) 
+
+firstname = "Rishu";
+lastname = "Tiwari";
+address = "Chanda";
+console.log("Hello",firstname,lastname,address) 
